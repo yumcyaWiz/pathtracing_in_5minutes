81541185e9391a668ec5dff0b8b9a1e65020b4d0
yumcyawiz <hashdolf1923@gmail.com>
2020-08-15 00:29:34 +0900

    impl vec3 class

--- main.cpp
+++ main.cpp
@@ -1,7 +1,84 @@
 // STL
+#include <cmath>
 #include <iostream>
 
 // extern
 #include <omp.h>
 
-int main() { return 0; }
+// base computation type
+using Real = float;
+
+// 3-dimensional vector class
+class Vec3 {
+ public:
+  Real x;
+  Real y;
+  Real z;
+
+  Vec3() { x = y = z = 0; }
+  Vec3(Real _x) { x = y = z = _x; }
+  Vec3(Real _x, Real _y, Real _z) : x(_x), y(_y), z(_z) {}
+
+  Real length() const { return std::sqrt(x * x + y * y + z * z); }
+  Real length2() const { return x * x + y * y + z * z; }
+};
+
+// Vec3 operators
+Vec3 operator+(const Vec3& v1, const Vec3& v2) {
+  return Vec3(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z);
+}
+Vec3 operator+(const Vec3& v, Real k) {
+  return Vec3(v.x + k, v.y + k, v.z + k);
+}
+Vec3 operator+(Real k, const Vec3& v) {
+  return Vec3(k + v.x, k + v.y, k + v.z);
+}
+
+Vec3 operator-(const Vec3& v1, const Vec3& v2) {
+  return Vec3(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z);
+}
+Vec3 operator-(const Vec3& v, Real k) {
+  return Vec3(v.x - k, v.y - k, v.z - k);
+}
+Vec3 operator-(Real k, const Vec3& v) {
+  return Vec3(k - v.x, k - v.y, k - v.z);
+}
+
+Vec3 operator*(const Vec3& v1, const Vec3& v2) {
+  return Vec3(v1.x * v2.x, v1.y * v2.y, v1.z * v2.z);
+}
+Vec3 operator*(const Vec3& v, Real k) {
+  return Vec3(v.x * k, v.y * k, v.z * k);
+}
+Vec3 operator*(Real k, const Vec3& v) {
+  return Vec3(k * v.x, k * v.y, k * v.z);
+}
+
+Vec3 operator/(const Vec3& v1, const Vec3& v2) {
+  return Vec3(v1.x / v2.x, v1.y / v2.y, v1.z / v2.z);
+}
+Vec3 operator/(const Vec3& v, Real k) {
+  return Vec3(v.x / k, v.y / k, v.z / k);
+}
+Vec3 operator/(Real k, const Vec3& v) {
+  return Vec3(k / v.x, k / v.y, k / v.z);
+}
+
+Real dot(const Vec3& v1, const Vec3& v2) {
+  return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
+}
+Vec3 cross(const Vec3& v1, const Vec3& v2) {
+  return Vec3(v1.y * v2.z - v1.z * v2.y, v1.z * v2.x - v1.x * v2.z,
+              v1.x * v2.y - v1.y * v2.x);
+}
+
+std::ostream& operator<<(std::ostream& stream, const Vec3& v) {
+  stream << "(" << v.x << ", " << v.y << ", " << v.z << ")";
+  return stream;
+}
+
+int main() {
+  Vec3 v1(1, 0, 0), v2(0, 1, 0);
+  std::cout << v1 + v2 << std::endl;
+  return 0;
+}
